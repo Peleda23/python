c0849eace2fcfc53fbb9799a14a1697f1afbf3a6
Peleda23 <kosmosas23@gmail.com>
2021-11-30 06:25:03 +0200

    learning fucntions

--- day8.py
+++ day8.py
@@ -13,7 +13,29 @@
 
 #Function with more than 1 input
 
-def greet_with(name, location):
-    print(f'Hello {name} from {location}.')
+# def greet_with(name, location):
+#     print(f'Hello {name} from {location}.')
 
-greet_with('Tomas', 'Klaipeda')
+# greet_with('Tomas', 'Klaipeda')
+
+#Write your code below this line 👇
+import math
+def paint_calc(height, width, cover):
+     number_of_cans = (height * width)/cover
+     number_of_cans = math.ceil(number_of_cans)
+     print(number_of_cans)
+
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)

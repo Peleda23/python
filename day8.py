af407e21cb97ae8d1e44718f02878c02cd26f4b3
Peleda23 <kosmosas23@gmail.com>
2021-12-02 06:34:05 +0200

    Started working on caesar chiper project

--- day8.py
+++ day8.py
@@ -19,23 +19,32 @@
 # greet_with('Tomas', 'Klaipeda')
 
 #Write your code below this line 👇
-import math
-def paint_calc(height, width, cover):
-     number_of_cans = (height * width)/cover
-     number_of_cans = math.ceil(number_of_cans)
-     print(number_of_cans)
-
-
-
-
-
-
+# import math
+# def paint_calc(height, width, cover):
+#      number_of_cans = (height * width)/cover
+#      number_of_cans = math.ceil(number_of_cans)
+#      print(number_of_cans)
 
 #Write your code above this line 👆
 # Define a function called paint_calc() so that the code below works.   
 
 # 🚨 Don't change the code below 👇
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+def prime_checker(number):
+     is_prime = True
+     for i in range(2, number):
+          if number % i == 0:
+               is_prime = False
+     if is_prime:
+          print("It's a prime number.")
+     else:
+          print("It's not a prime number")
+
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
